e234b99cc4c9aaf16fbf09c7e99115daff5d8cf9
Eslam-Sayed7 <eslam.sayed.ed@gmail.com>
2025-10-14 17:22:21 +0300

    rm/mock_api

--- Mock_API/app/main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from .routes import router
-
-app = FastAPI(title="UI Simulation Server")
-app.include_router(router)
-@app.get("/")
-def home():
-    return {"message": "UI Simulation Server is running"}
